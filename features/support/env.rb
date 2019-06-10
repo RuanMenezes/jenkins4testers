46c7290bb14d55c1372e86b5121af0589e90082c
Fernando Papito <eu@papito.io>
2019-06-10 00:19:48 -0300

    alpine

--- features/support/env.rb
+++ features/support/env.rb
@@ -11,17 +11,25 @@ CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_T
 
 case ENV["BROWSER"]
 when "firefox"
-  @driver = :selenium
+  Capybara.default_driver = :selenium
 when "chrome"
-  @driver = :selenium_chrome
+  Capybara.default_driver = :selenium_chrome
 when "headless"
-  @driver = :selenium_chrome_headless
+  Capybara.register_driver :selenium do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+      opts.add_argument "--headless"
+      opts.add_argument "--disable-gpu"
+      opts.add_argument "--no-sandbox"
+      opts.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  Capybara.default_driver = :selenium
 else
   puts "Invalid browser"
 end
 
 Capybara.configure do |config|
-  config.default_driver = @driver
   config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
